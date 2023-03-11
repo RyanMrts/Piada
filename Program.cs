f4a1fca481e9255cd000ac39cb610dc93e6e9ef7
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-10 21:30:39 -0300

    finalizando exercicio

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Aperte Enter para ver a Piada");
+Console.ReadLine();
+Console.WriteLine("O que é uma mulher velha sem relógio?");
+Thread. Sleep(2000);
+Console.WriteLine("Uma sem hora.");
